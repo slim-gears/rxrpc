eafad0acc6682423c111d6f4893273eb1f3fc353
denis-itskovich <denis.itskovich@dell.com>
2018-09-12 15:17:14 +0300

    Fixing meta endpoint annotations

--- rxrpc-apt/src/test/resources/input/SampleGenericMetaEndpoint.java
+++ rxrpc-apt/src/test/resources/input/SampleGenericMetaEndpoint.java
@@ -8,7 +8,7 @@ import io.reactivex.Observable;
 
 @RxRpcGenerate(
         className = "SampleGenericMetaEndpoint_Of_${T}",
-        annotation = @RxRpcEndpoint("sampleGenericMetaEndpoint_of_${T}"),
+        annotation = @RxRpcEndpoint(value = "sampleGenericMetaEndpoint_of_${T}", options = "rxrpc.java.server=true"),
         value = {
                 @RxRpcGenerate.Endpoint(
                         className = "SampleGenericMetaEndpointWithSpecificName",
