b10b7e075ac070bd0c9475ea5114b135fb06a93c
brusid <dmitry.brusilovsky@emc.com>
2019-02-26 10:48:40 +0200

    Fixing failing tests

--- rxrpc-apt-typescript/src/test/resources/output/module.ts
+++ rxrpc-apt-typescript/src/test/resources/output/module.ts
@@ -1,11 +1,10 @@
 import {InjectionToken, Injector, ModuleWithProviders, NgModule, Type} from '@angular/core';
-import {RxRpcClient, RxRpcClientModule, RxRpcInvoker, RxRpcTransport} from 'ng-rxrpc';
+import {RxRpcClient, RxRpcInvoker, RxRpcTransport} from 'ng-rxrpc';
 import { SampleEndpointClient } from './sample-endpoint-client';
 
 export const RxRpcGeneratedClientModuleRXRPC_INVOKER = new InjectionToken<RxRpcInvoker>('RxRpcGeneratedClientModule.RxRpcInvoker');
 
 @NgModule({
-    imports: [ RxRpcClientModule ],
     providers: [
         SampleEndpointClient.provider(RxRpcGeneratedClientModuleRXRPC_INVOKER)
     ]
