16151cf3048e304a58658b4127f240c3d96198a8
denis-itskovich <denis.itskovich@dell.com>
2022-03-24 12:41:09 +0200

    [rxrpc-apt-typescript] Fixing tests

--- rxrpc-apt-typescript/src/test/resources/output/rx-rpc-generated-client-module.ts
+++ rxrpc-apt-typescript/src/test/resources/output/rx-rpc-generated-client-module.ts
@@ -1,5 +1,5 @@
 import {InjectionToken, ModuleWithProviders, NgModule, Type} from '@angular/core';
-import {RxRpcClient, RxRpcInvoker, RxRpcTransport} from 'rxrpc-js';
+import {RxRpcClient, RxRpcInvoker, RxRpcTransport, RxRpcClientOptions} from 'rxrpc-js';
 import { SampleEndpointClient } from './sample-endpoint-client';
 
 @NgModule({
@@ -10,18 +10,18 @@ import { SampleEndpointClient } from './sample-endpoint-client';
 export class RxRpcGeneratedClientModule {
     public static readonly RxRpcInvoker = new InjectionToken<RxRpcInvoker>('RxRpcGeneratedClientModule.RxRpcInvoker');
 
-    public static withTransport(transportToken: Type<RxRpcTransport>|InjectionToken<RxRpcTransport>): ModuleWithProviders<RxRpcGeneratedClientModule> {
+    public static withTransport(transportToken: Type<RxRpcTransport>|InjectionToken<RxRpcTransport>, options?: RxRpcClientOptions): ModuleWithProviders<RxRpcGeneratedClientModule> {
         return {
             ngModule: RxRpcGeneratedClientModule,
             providers: [{
                 provide: RxRpcGeneratedClientModule.RxRpcInvoker,
-                useFactory: RxRpcGeneratedClientModule.invokerFactory,
+                useFactory: transport => RxRpcGeneratedClientModule.invokerFactory(transport, options),
                 deps: [transportToken]
             }]
         };
     }
 
-    public static invokerFactory(transport: RxRpcTransport) {
-        return new RxRpcClient(transport);
+    public static invokerFactory(transport: RxRpcTransport, options?: RxRpcClientOptions) {
+        return new RxRpcClient(transport, options);
     }
 }
